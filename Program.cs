8b180d104be83f3a6594de33729044c8118675a7
TheMasterKev <36552239+TheMasterKev@users.noreply.github.com>
2019-10-01 21:34:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int scelta;
+            alunno alunno1 = new alunno();
+            alunno alunno2 = new alunno();
+            alunno alunno3 = new alunno();
+
+            alunno1.Nome = "Alberto Bufalo";
+            alunno2.Nome = "Claudio Dermatologo";
+            alunno3.Nome = "Eusebio Ferrari";
+
+            do
+            {
+                scelta = menu(alunno1, alunno2, alunno3);
+                switch(scelta)
+                {
+                    case 1:
+                        alunno1.voto();
+                        break;
+
+                    case 2:
+                        alunno2.voto();
+                        break;
+
+                    case 3:
+                        alunno3.voto();
+                        break;
+
+                }
+            } while (scelta != 0);
+        }
+
+        static int menu(alunno alunno1, alunno alunno2, alunno alunno3)
+        {
+            int sc;
+            if(alunno1.media == 0)
+                Console.WriteLine("1. {0} Inserire voti - Media: non calcolata", alunno1.Nome);
+            else
+            Console.WriteLine("1. {0} Inserire voti - Media: {1}", alunno1.Nome, alunno1.Media());
+            if (alunno2.media == 0)
+                Console.WriteLine("1. {0} Inserire voti - Media: non calcolata", alunno2.Nome);
+            else
+                Console.WriteLine("2. {0} Inserire voti - Media: {1}", alunno2.Nome, alunno2.Media());
+            if (alunno3.media == 0)
+                Console.WriteLine("1. {0} Inserire voti - Media: non calcolata", alunno3.Nome);
+            else
+                Console.WriteLine("3. {0} Inserire voti - Media: {1}", alunno3.Nome, alunno3.Media());
+            Console.WriteLine("0. Fine");
+            sc = int.Parse(Console.ReadLine());
+            return sc;
+        }
+    }
+}
---
+++ alunno.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_10
+{
+    class alunno
+    {
+        private string nome;
+        private int[] voti = new int[3];
+        public float media;
+        public int somma;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public void voto()
+        {
+            int cnt;
+
+            for (cnt = 0; cnt < 3; cnt++)
+            {
+                do
+                {
+                    Console.WriteLine("Inserire {0} voto: ", cnt + 1);
+                    voti[cnt] = int.Parse(Console.ReadLine());
+                } while (voti[cnt] < 0 || voti[cnt] > 10);
+
+                if(voti[cnt] < 0 || voti[cnt] > 10)
+                {
+                    Console.WriteLine("Il numero inserito non è valido!!");
+                }
+            }
+            media = Somma() / 3;
+        }
+
+        private int Somma()
+        {
+            int cnt;
+
+            for (cnt = 0; cnt < 3; cnt++)
+            {
+                somma = somma + voti[cnt];
+            }
+            return somma;
+        }
+
+        public float Media()
+        {
+
+            media = somma / 3;
+            return media;
+        }
+    }
+}
